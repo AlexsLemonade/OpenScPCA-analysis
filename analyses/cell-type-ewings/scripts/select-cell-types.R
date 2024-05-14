920fd0abbb530e23cf2c298d12f25d34997b3bf8
Ally Hawkins <ally.hawkins@ccdatalab.org>
2024-05-14 16:31:22 -0500

    update script for selecting reference cells

---
+++ analyses/cell-type-ewings/scripts/select-cell-types.R
@@ -0,0 +1,113 @@
+#!/usr/bin/env Rscript
+
+# This script is used to generate a table of reference cells for a given ScPCA libray that are annotated by 
+# SingleR and/or CellAssign to have a specific label
+# The output is a table that contains columns for barcodes, reference cell class, 
+# and then columns for each of the original cell type annotations for that cell 
+# The `reference_cell_class` column will contain either tumor or normal 
+
+project_root <- here::here()
+renv::load(project_root)
+
+suppressPackageStartupMessages({
+  library(optparse)
+  library(SingleCellExperiment)
+})
+
+option_list <- list(
+  make_option(
+    opt_str = c("--sce_file"),
+    type = "character",
+    help = "Path to RDS file containing a processed SingleCellExperiment object from scpca-nf"
+  ),
+  make_option(
+    opt_str = c("--normal_cells"),
+    type = "character",
+    default = NULL,
+    help = "Comma separated list of cell types to pull from annotations present and save as normal cells."
+  ),
+  make_option(
+    opt_str = c("--tumor_cells"),
+    type = "character",
+    default = NULL,
+    help = "Comma separated list of cell types to pull from annotations present and save as tumor cells."
+  ),
+  make_option(
+    opt_str = c("--output_dir"),
+    type = "character",
+    help = "path to folder to save output cell file"
+  ),
+  make_option(
+    opt_str = c("--output_filename"),
+    type = "character",
+    help = "filename to use for saved table of cell barcodes. Will be saved inside `<output_dir>`"
+  )
+)
+
+# Parse options
+opt <- parse_args(OptionParser(option_list = option_list))
+
+# Set up -----------------------------------------------------------------------
+
+# make sure path to sce file exists
+stopifnot("sce_file does not exist" = file.exists(opt$sce_file))
+
+# check that either normal or tumor cell types are provided 
+stopifnot("Either normal_cells or tumor_cells must be provided" = 
+            !is.null(opt$normal_cells) || !is.null(opt$tumor_cells))
+
+# read in sce file
+sce <- readr::read_rds(opt$sce_file)
+
+# create output directory if not already present
+fs::dir_create(opt$output_dir)
+output_cell_file <- file.path(opt$output_dir, opt$output_filename)
+
+
+# Create table of reference cells ----------------------------------------------
+
+# possible annotation columns 
+annotation_columns <- c(
+  "singler_celltype_annotation",
+  "cellassign_celltype_annotation"
+)
+
+# get dataframe with barcodes and associated annotation 
+coldata_df <- colData(sce) |>
+  as.data.frame() |> 
+  dplyr::select(barcodes, all_of(annotation_columns)) |> 
+  tidyr::pivot_longer(all_of(annotation_columns),
+                      names_to = "celltype_method",
+                      values_to = "celltype")
+
+# get list of normal and tumor cell types to save as refernences 
+if(!is.null(opt$normal_cells)){
+  normal_cell_types <- stringr::str_split(opt$normal_cells, pattern = ",") |>
+    unlist()
+} else {
+  normal_cell_types <- ""
+}
+
+if(!is.null(opt$tumor_cells)){
+  tumor_cell_types <- stringr::str_split(opt$tumor_cells, pattern = ",") |>
+    unlist()
+} else {
+  tumor_cell_types <- ""
+}
+
+# label cells as either tumor or normal based on which cell type was annotated 
+ref_table_df <- coldata_df |> 
+  dplyr::mutate(reference_cell_class = dplyr::case_when(
+    celltype %in% normal_cell_types ~ "Normal",
+    celltype %in% tumor_cell_types ~ "Tumor"
+  )) |> 
+  # remove any that weren't tumor or normal 
+  dplyr::filter(!is.na(reference_cell_class)) |> 
+  # get one row per barcode and one column for each annotation type 
+  tidyr::pivot_wider(values_from = celltype,
+                     names_from = celltype_method)
+
+# export table of reference cells 
+readr::write_tsv(ref_table_df, output_cell_file)
+
+
