7755366f2332075bdc682f072700168089f14977
Ally Hawkins <ally.hawkins@ccdatalab.org>
2024-05-21 13:18:09 -0500

    use reference cell table

--- analyses/cell-type-ewings/scripts/run-copykat.R
+++ analyses/cell-type-ewings/scripts/run-copykat.R
@@ -17,22 +17,23 @@ option_list <- list(
     help = "Path to RDS file containing a processed SingleCellExperiment object from scpca-nf"
   ),
   make_option(
-    opt_str = c("--normal_cells"),
+    opt_str = c("--reference_cell_file"),
     type = "character",
     default = NULL,
-    help = "Optional file that contains a list of cell barcodes that correspond to normal cells and will be used as a reference"
+    help = "Optional file that contains a table of cell barcodes. 
+      Any cells with Normal in the reference_cell_class column will be used as a reference for CopyKAT"
   ),
   make_option(
     opt_str = c("--results_dir"),
     type = "character",
     default = NULL,
-    help = "Folder within `--copykat_results_prefix/library_id` to save copyKAT results"
+    help = "Full path to folder to save final CopyKAT results"
   ),
   make_option(
-    opt_str = c("--copykat_results_prefix"),
+    opt_str = c("--scratch_dir"),
     type = "character",
-    default = file.path(project_root, "results", "copykat"),
-    help = "path to folder where all copykat results live"
+    default = file.path(project_root, "scratch", "copykat"),
+    help = "path to folder where all intermediate CopyKAT results live"
   ),
   make_option(
     opt_str = c("-t", "--threads"),
@@ -60,22 +61,29 @@ sce <- readr::read_rds(opt$sce_file)
 library_id <- metadata(sce)$library_id
 
 # contstruct and create output folder if not already present 
-output_dir <- file.path(opt$copykat_results_prefix, library_id, opt$results_dir) 
-fs::dir_create(output_dir)
+fs::dir_create(opt$output_dir)
 
-# change working directory of the script to the output directory
+# path to output copykat object 
+copykat_output_file <- file.path(opt$results_dir, glue::glue("{library_id}_final-copykat.rds"))
+
+# define scratch directory 
+scratch_dir <- file.path(opt$scratch_dir, library_id)
+
+# change working directory of the script to the scratch directory
 # this ensures copykat files get saved to the right location
 # there is no option to specify an output directory when running copykat
-setwd(output_dir)
+setwd(scratch_dir)
 
 # Run CopyKAT ------------------------------------------------------------------
 
 # if normal cells exist then read in and set option for running copyKAT
-if(!is.null(opt$normal_cells)){
+if(!is.null(opt$reference_cell_file)){
   
   # make sure normal cells file exists 
-  stopifnot("normal_cells file does not exist" = file.exists(opt$normal_cells))
-  normal_cells <- readLines(opt$normal_cells)
+  stopifnot("reference file does not exist" = file.exists(opt$reference_cell_file))
+  normal_cells <- readr::read_tsv(opt$reference_cell_file) |> 
+    dplyr::filter(reference_cell_class == "Normal") |> 
+    dplyr::pull(barcodes)
   
   # check that all normal cells are in colnames of sce 
   stopifnot("All barcodes in the normal_cells file are not found in the sce object" = 
@@ -97,3 +105,5 @@ copykat_result <- copykat(
   n.cores = opt$threads
 )
 
+# Save outputs 
+readr::write_rds(copykat_result, copykat_output_file)
