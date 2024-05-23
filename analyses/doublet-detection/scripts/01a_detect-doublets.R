53f7c9e7b54d17cb4c5769069d92d5c15bb46daa
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2024-05-23 15:43:23 -0400

    reproducibilify scdblfinder script

--- analyses/doublet-detection/scripts/01a_detect-doublets.R
+++ analyses/doublet-detection/scripts/01a_detect-doublets.R
@@ -1,30 +1,12 @@
 #!/usr/bin/env Rscript
 
+
+# Load libraries and renv environment ------
 library(SingleCellExperiment)
 library(optparse)
 
-# Setup ------
-
-# Parse options
-option_list <- list(
-  make_option(
-    c("-c", "--cores"),
-    type = "character",
-    help = "Number of cores to use during scDblFinder inference."
-  )
-)
-opts <- parse_args(OptionParser(option_list = option_list))
-
-
-# Define directories
-module_base <- file.path(
-  rprojroot::find_root(rprojroot::is_git_root),
-  "analyses",
-  "doublet-detection"
-)
-data_dir <- file.path(module_base, "scratch", "benchmark_datasets")
-result_dir <- file.path(module_base, "results", "benchmark_results")
-fs::dir_create(result_dir)
+project_root <- here::here()
+renv::load(project_root)
 
 
 # Functions -----------
@@ -33,11 +15,11 @@ fs::dir_create(result_dir)
 #'
 #' @param sce SCE object to run scDblFinder on
 #' @param cores Number of cores to specify to BiocParallel::MulticoreParam(
-#' @param sample_var Required for multiplexed data, the SCE colData variable that indicates sample of origin
-#' @param random_seed Required for multiplexed data, a random seed to ensure reproducibility when running on multiplexed data.
+#' @param sample_var Required for SCE object with multiple samples, the SCE colData variable that indicates sample of origin
+#' @param random_seed Required for SCE object with multiple samples, a random seed to ensure reproducibility when running on multiple samples
 #' @param ... Additional arguments to pass to scDblFinder::scDblFinder
 #'
-#' @return SCE object with scDblFinder inferences
+#' @return Data frame object with scDblFinder inferences
 run_scdblfinder <- function(sce,
                             cores = 4,
                             sample_var = NULL,
@@ -45,13 +27,14 @@ run_scdblfinder <- function(sce,
                             ...) {
 
   if (is.null(sample_var)) {
-    # not multiplexed
-    sce <- scDblFinder::scDblFinder(sce,
-                                    BPPARAM = BiocParallel::MulticoreParam(cores),
-                                    ...)
+    # SCE with single sample
+    result_df <- scDblFinder::scDblFinder(
+      sce,
+      BPPARAM = BiocParallel::MulticoreParam(cores),
+      returnType = "table", # return df, not sce
+      ...
+    )
   } else {
-
-
     if (is.null(random_seed)) {
       # See section 1.5.6: https://bioconductor.org/packages/3.19/bioc/vignettes/scDblFinder/inst/doc/scDblFinder.html#usage
       stop("If multiple samples are present in the input SCE object, a random seed must be provided for reproducibility.")
@@ -62,29 +45,82 @@ run_scdblfinder <- function(sce,
     } else {
       bp <- BiocParallel::MulticoreParam(cores, RNGseed = random_seed)
     }
-    sce <- scDblFinder::scDblFinder(sce,
-                                    samples = sample_var,
-                                    BPPARAM = bp,
-                                    ...)
+    result_df <- scDblFinder::scDblFinder(
+      sce,
+      samples = sample_var,
+      BPPARAM = bp,
+      returnType = "table", # return df, not sce
+      ...
+    )
   }
 
-  return(sce)
+  result_df <- result_df |>
+    as.data.frame() |>
+    tibble::rownames_to_column("barcodes")
+
+  return(result_df)
 }
 
-# Infer doublets --------------
 
-# Benchmark dataset names
-datanames <- c("hm-6k", "pbmc-1B-dm", "pdx-MULTI", "HMEC-orig-MULTI")
+# Parse options --------
 
-# Detect doublets and export SCE file with inferences
-datanames |>
-  purrr::walk(
-    \(dataname) {
-      input_sce_file <- file.path(data_dir, glue::glue("{dataname}_sce.rds"))
-      output_sce_file <- file.path(result_dir, glue::glue("{dataname}_sce.rds"))
-
-      readRDS(input_sce_file) |>
-        run_scdblfinder(cores = opts$cores) |>
-        readr::write_rds(output_sce_file)
-    }
+option_list <- list(
+  make_option(
+    "--dataset_name",
+    type = "character",
+    help = "Name of dataset to process, where the associated file is expected to be named `{name}_sce.rds`"
+  ),
+  make_option(
+    "--data_dir",
+    type = "character",
+    default = "",
+    help = "The directory containing the input RDS file."
+  ),
+  make_option(
+    "--results_dir",
+    type = "character",
+    default = "",
+    help = "The directory to export TSV file with doublet inferences."
+  ),
+  make_option(
+    "--cores",
+    type = "integer",
+    default = 4,
+    help = "Number of cores to use during scDblFinder inference."
+  ),
+  make_option(
+    "--random_seed",
+    type = "integer",
+    default = 2024,
+    help = "Random seed. Default is 2024."
   )
+)
+opts <- parse_args(OptionParser(option_list = option_list))
+
+# Check input arguments
+if (opts$dataset_name == "") {
+  stop("Must provide a dataset name with --dataset_name.")
+}
+if (!dir.exists(opts$data_dir)) {
+  stop("Must provide an input path to data files with --data_dir.")
+}
+if (opts$results_dir == "") {
+  stop("Must provide an output path for results with --results_dir.")
+}
+
+fs::dir_create(opts$results_dir)
+set.seed(opts$random_seed)
+
+
+# Detect doublets and export TSV file with inferences -----
+input_sce_file <- file.path(opts$data_dir, glue::glue("{opts$dataset_name}_sce.rds"))
+if (!file.exists(input_sce_file)) {
+  stop(
+    glue::glue("Could not find input file, expected at: `{input_sce_file}`.")
+  )
+}
+output_tsv_file <- file.path(opts$results_dir, glue::glue("{opts$dataset_name}_scdblfinder.tsv"))
+
+readRDS(input_sce_file) |>
+  run_scdblfinder(cores = opts$cores) |>
+  readr::write_tsv(output_tsv_file)
