43e7f49b8d0be864c532851464810e995abb5110
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2024-05-23 16:15:51 -0400

    module run script massively updated for modularity

--- analyses/doublet-detection/run_doublet-detection.sh
+++ analyses/doublet-detection/run_doublet-detection.sh
@@ -8,18 +8,34 @@ set -euo pipefail
 MODULE_DIR=$(dirname "${BASH_SOURCE[0]}")
 cd ${MODULE_DIR}
 
-# Create directories
-mkdir -p scratch/benchmark_datasets
-mkdir -p results/benchmark_results
+###########################################################################################
+########## Step 1: Benchmark doublet detection methods on ground truth datasets ###########
+###########################################################################################
 
+# Create benchmark directories
+BENCH_DATA_DIR_RAW=scratch/benchmark_datasets
+BENCH_DATA_DIR=${BENCH_DATA_DIR_RAW}/formatted
+BENCH_RESULT_DIR=results/benchmark_results
+mkdir -p ${BENCH_DATA_DIR} # also creates ${BENCH_DATA_DIR_RAW}
+mkdir -p ${BENCH_RESULT_DIR}
 
-# Step 1: Benchmark doublet detection methods on ground truth datasets -----------
+# define benchmarking datasets to use
+bench_datasets=("hm-6k" "pbmc-1B-dm" "pdx-MULTI" "HMEC-orig-MULTI")
 
-# Download and format data
-./scripts/00a_download-benchmark-data.sh
+# Download and unzip `real_datasets.zip` archive from https://doi.org/10.5281/zenodo.4562782
+wget https://zenodo.org/records/4562782/files/real_datasets.zip
+unzip real_datasets.zip -d ${BENCH_DATA_DIR_RAW}
+rm real_datasets.zip
 
-# Infer doublets with scDblFinder
-./scripts/01a_detect-doublets.R
+for dataset in "${bench_datasets[@]}"; do
 
-# Infer doublets with scrublet 01b_detect-doublets.py
-./scripts/01b_detect-doublets.py
+    # Read raw downloaded data and export SCE, AnnData files
+    ./scripts/00_format-benchmark-data.R --dataset_name ${dataset} --input_dir ${BENCH_DATA_DIR_RAW} --output_dir ${BENCH_DATA_DIR}
+
+    # Infer doublets with scDblFinder
+    ./scripts/01a_detect-doublets.R --dataset_name ${dataset} --data_dir ${BENCH_DATA_DIR} --results_dir ${BENCH_RESULT_DIR}
+
+    # Infer doublets with scrublet
+    ./scripts/01b_detect-doublets.py --dataset_name ${dataset} --data_dir ${BENCH_DATA_DIR} --results_dir ${BENCH_RESULT_DIR}
+
+done
