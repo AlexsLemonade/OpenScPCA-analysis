5a21d11cda4a23988a8cb1591d6c9dc33a5eae77
Stephanie Spielman <stephanie.spielman@gmail.com>
2024-07-12 15:32:49 -0400

    Update analyses/doublet-detection/scripts/01a_run-scdblfinder.R
    Co-authored-by: Ally Hawkins <54039191+allyhawkins@users.noreply.github.com>

--- analyses/doublet-detection/scripts/01a_run-scdblfinder.R
+++ analyses/doublet-detection/scripts/01a_run-scdblfinder.R
@@ -94,7 +94,7 @@ option_list <- list(
     "--benchmark",
     action = "store_true",
     default = FALSE,
-    help = "Whether the script is being run as part of the doublet benchmarking analysis. Different columns will be output in this mode."
+    help = "Whether the script is being run as part of the doublet benchmarking analysis. If this flag is invoked, the `cxds_score` column will be returned along with the `score` and `class` columns that are always returned. " 
   ),
   make_option(
     "--sample_var",
