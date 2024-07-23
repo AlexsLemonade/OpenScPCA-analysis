dca6ed2217b0e25520a426698aa92ffce8e62e6f
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-07-23 15:10:10 -0400

    add full script

--- analyses/metacells/scripts/run-seacells.py
+++ analyses/metacells/scripts/run-seacells.py
@@ -5,7 +5,7 @@ Seacells Analysis Script
 Joshua Shapiro
 2024-07-18
 
-This script runs the SEACell algorithm on the given dataset.
+This script runs the SEACells algorithm on the given dataset.
 """
 
 # Load modules
@@ -24,8 +24,48 @@ import contextlib
 import pathlib
 
 import anndata
+import numpy as np
+import scanpy
 import SEACells
 import session_info
+
+
+def convert_adata(adata: anndata.AnnData) -> anndata.AnnData:
+    """
+    Convert an ScPCA AnnData object to the formatting expected by scanpy and SEACells.
+    """
+    # put the highly variable genes in a column of adata.var
+    adata.var["highly_variable"] = adata.var.gene_ids.isin(
+        adata.uns["highly_variable_genes"]
+    )
+
+    # recompute principal components and umap (adds additional metadata)
+    scanpy.tl.pca(adata, n_comps=50, mask_var="highly_variable")
+    scanpy.tl.umap(adata)
+
+    return adata
+
+
+def run_seacells(adata: anndata.AnnData, cell_ratio: float = 75) -> anndata.AnnData:
+    """
+    Run the SEACells algorithm on the given dataset.
+    """
+    n_metacells = round(adata.n_obs / cell_ratio)
+    n_eigs = 10  # number of eigenvalues for initialization
+    # initialize the SEACells model
+    model = SEACells.core.SEACells(
+        adata,
+        build_kernel_on="X_pca",
+        n_SEACells=n_metacells,
+        n_waypoint_eigs=n_eigs,
+        convergence_epsilon=1e-5,
+    )
+    # initialize and fit model
+    model.construct_kernel_matrix()
+    model.initialize_archetypes()
+    model.fit(min_iter=10, max_iter=50)
+
+    return adata
 
 
 def main() -> None:
@@ -35,12 +75,24 @@ def main() -> None:
     parser.add_argument(
         "datafile", type=pathlib.Path, help="The input data in H5AD format."
     )
+    parser.add_argument(
+        "seed",
+        type=int,
+        help="The random seed to use for reproducibility.",
+        default=2024,
+    )
     parser.add_argument("output", type=pathlib.Path, help="The output file path.")
     parser.add_argument("logfile", type=pathlib.Path, help="File path for log outputs")
 
     args = parser.parse_args()
 
-    adata = AnnData.read(args.datafile)
+    # set seed for reproducibility
+    np.random.seed(args.seed)
+
+    adata = scanpy.read(args.datafile)
+
+    adata = convert_adata(adata)
+    adata = run_seacells(adata)
 
     # As the last step, record the versions of the modules and dependencies
     # that were used in this analysis
