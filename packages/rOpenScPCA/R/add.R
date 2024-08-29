8ed1557b8eba39bbdb9e835810eb0e622aa66490
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2024-08-29 16:32:18 -0400

    quick function just to test the gha

---
+++ packages/rOpenScPCA/R/add.R
@@ -0,0 +1,10 @@
+#' Add
+#'
+#' @param x A numeric
+#' @param y A numeric
+#'
+#' @return Sum of numerics
+#' @export
+add <- function(x, y) {
+  return(x + y)
+}
---
+++ packages/rOpenScPCA/tests/testthat/test-add.R
@@ -0,0 +1,3 @@
+test_that("add function works", {
+  expect_equal(add(1, 5), 6)
+})
