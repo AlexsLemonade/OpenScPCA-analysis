e0c1b942f40501225edf4d4ed55fd1084c13812e
Jaclyn Taroni <19534205+jaclyn-taroni@users.noreply.github.com>
2024-09-01 12:19:23 -0400

    Fix indentation and repeated step from merge conflict

--- analyses/cell-type-wilms-tumor-06/00_run_workflow.R
+++ analyses/cell-type-wilms-tumor-06/00_run_workflow.R
@@ -67,23 +67,15 @@ for (sample_id in metadata$scpca_sample_id) {
                     output_dir = file.path(notebook_output_dir,  sample_id))
 
   if (!running_ci) {
-  # Label transfer from the Cao reference using Azimuth
-  rmarkdown::render(input = file.path(notebook_template_dir, "02a_label-transfer_fetal_full_reference_Cao.Rmd"),
-                    params = list(scpca_project_id = project_id, sample_id = sample_id),
-                    output_format = "html_document",
-                    output_file = paste0("02a_fetal_all_reference_Cao_", sample_id, ".html"),
-                    output_dir = file.path(notebook_output_dir, sample_id))
-  
-  # Label transfer from the Stewart reference using Seurat
-  rmarkdown::render(input = file.path(notebook_template_dir, "02b_label-transfer_fetal_kidney_reference_Stewart.Rmd"),
-                    params = list(scpca_project_id = project_id, sample_id = sample_id),
-                    output_format = "html_document",
-                    output_file = paste0("02b_fetal_kidney_reference_Stewart_", sample_id, ".html"),
-                    output_dir = file.path(notebook_output_dir, sample_id))
-  
+    # Label transfer from the Cao reference using Azimuth
+    rmarkdown::render(input = file.path(notebook_template_dir, "02a_label-transfer_fetal_full_reference_Cao.Rmd"),
+                      params = list(scpca_project_id = project_id, sample_id = sample_id),
+                      output_format = "html_document",
+                      output_file = paste0("02a_fetal_all_reference_Cao_", sample_id, ".html"),
+                      output_dir = file.path(notebook_output_dir, sample_id))
 
     # Label transfer from the Stewart reference using Seurat
-    rmarkdown::render(input = file.path(module_base, "notebook_template", "02b_label-transfer_fetal_kidney_reference_Stewart.Rmd"),
+    rmarkdown::render(input = file.path(notebook_template_dir, "02b_label-transfer_fetal_kidney_reference_Stewart.Rmd"),
                       params = list(scpca_project_id = project_id, sample_id = sample_id),
                       output_format = "html_document",
                       output_file = paste0("02b_fetal_kidney_reference_Stewart_", sample_id, ".html"),
