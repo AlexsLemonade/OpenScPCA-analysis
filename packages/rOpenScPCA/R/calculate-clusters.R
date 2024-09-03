116c73d54924b1da2d0373bb0c2bbc54f2844a06
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2024-09-03 14:30:58 -0400

    add bluster and roxygen. Update column name to cell_id

--- packages/rOpenScPCA/R/calculate-clusters.R
+++ packages/rOpenScPCA/R/calculate-clusters.R
@@ -1,3 +1,21 @@
+#' Calculate graph-based clusters from a provided matrix
+#'
+#' @param mat Matrix, usually of PCs, where each row is a cell. Matrix must have rownames of cell ids (e.g., barcodes)
+#' @param algorithm Clustering algorithm to use. Must be one of "louvain" (default), "walktrap", or "leiden"
+#' @param weighting Weighting scheme to use. Must be one of "jaccard" (default) or "rank"
+#' @param nn Number of nearest neighbors. Default is 10.
+#' @param resolution Resolution parameter used by louvain and leiden clustering only. Default is 1.
+#' @param objective_function Leiden-specific parameter for whether to use the Constant Potts Model ("CPM"; default) or "modularity"
+#' @param random_seed Random seed to set for clustering. Default is 2024.
+#' @param cluster_args List of additional arguments to pass to the clustering function.
+#'
+#' @return A data frame of cluster results with columns `cell_id` and `cluster`. Additional columns represent algorithm parameters.
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' # TODO WILL ADD EXAMPLES ONCE ARGS ARE FINALIZED
+#' }
 calculate_clusters <- function(
     mat,
     algorithm = "louvain",
@@ -58,7 +76,7 @@ calculate_clusters <- function(
   # TODO: Should this have _all_ parameters in cluster.args?
   return(
     tibble::tibble(
-      barcode = rownames(mat),
+      cell_id = rownames(mat),
       cluster = clusters,
       algorithm = algorithm,
       weighting = weighting,
