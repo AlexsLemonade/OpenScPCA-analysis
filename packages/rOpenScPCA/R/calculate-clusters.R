fe022d56bebf5d3a26661429bf93fe8f6d6dec46
Stephanie Spielman <stephanie.spielman@gmail.com>
2024-09-10 16:24:47 -0400

    expand docs for non-default args, and add number weighting

--- packages/rOpenScPCA/R/calculate-clusters.R
+++ packages/rOpenScPCA/R/calculate-clusters.R
@@ -1,17 +1,22 @@
 #' Calculate graph-based clusters from a provided matrix
 #'
 #' @param mat Matrix, usually of PCs, where each row is a cell. Matrix must have rownames of cell ids (e.g., barcodes)
-#' @param algorithm Clustering algorithm to use. Must be one of "louvain" (default), "walktrap", or "leiden"
-#' @param weighting Weighting scheme to use. Must be one of "jaccard" (default) or "rank"
+#' @param algorithm Clustering algorithm to use. Must be one of "louvain" (default), "walktrap", or "leiden".
+#'  Be aware that the default of "louvain" is different from the {bluster} package default of "walktrap". This difference is
+#'  because louvain clustering is more commonly-used in scRNA-seq analysis.
+#' @param weighting Weighting scheme to use. Must be one of "jaccard" (default), "rank", or "number"
+#'  Be aware that the default of "jaccard" is different from the {bluster} package default of "rank".
+#'  This difference is because jaccard weighting is more commonly-used in scRNA-seq analysis.
 #' @param nn Number of nearest neighbors. Default is 10.
 #' @param resolution Resolution parameter used by louvain and leiden clustering only. Default is 1.
 #' @param objective_function Leiden-specific parameter for whether to use the Constant Potts Model ("CPM"; default) or "modularity"
 #' @param seed Random seed to set for clustering. Default is 2024.
-#' @param cluster_args List of additional arguments to pass to the chosen clustering function. See igraph documentation for details: https://igraph.org/r/html/latest
-#'
+#' @param cluster_args List of additional arguments to pass to the chosen clustering function. Only single-length values will be used.
+#'   See igraph documentation for details on each clustering function: https://igraph.org/r/html/latest
 #' @return A data frame of cluster results with columns `cell_id` and `cluster`. Additional columns represent algorithm parameters
 #'   and include at least: `algorithm`, `weighting`, and `nn`. Louvain and leiden clustering will also include `resolution`, and
-#'   leiden clustering will further include `objective_function`. Any other values passed to `cluster_args` will also be included.
+#'   leiden clustering will further include `objective_function`.
+#'
 #' @export
 #'
 #' @examples
@@ -32,7 +37,7 @@
 calculate_clusters <- function(
     mat,
     algorithm = c("louvain", "walktrap", "leiden"),
-    weighting = c("jaccard", "rank"),
+    weighting = c("jaccard", "rank", "number"),
     nn = 10,
     resolution = 1, # louvain or leiden
     objective_function = c("CPM", "modularity"), # leiden only
@@ -59,6 +64,7 @@ calculate_clusters <- function(
       "`cluster_args` must be a named list." = is.list(cluster_args) && !("" %in% allNames(cluster_args))
     )
   }
+
 
   # Update cluster_args list with parameters that users can directly provide
   # note that clusterRows throws an error if this list has a param not used by the chosen algorithm
