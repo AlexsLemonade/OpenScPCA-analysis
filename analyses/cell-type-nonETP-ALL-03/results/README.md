f2aa909ce43f02cf2b5fdf1e257c0bf21eb1702a
UTSouthwesternDSSR <juiwan.loh@utsouthwestern.edu>
2024-10-01 21:47:31 +0000

    update for cell annotation

--- analyses/cell-type-nonETP-ALL-03/results/README.md
+++ analyses/cell-type-nonETP-ALL-03/results/README.md
@@ -7,4 +7,4 @@ cd /home/lightsail-user/OpenScPCA-analysis
 scripts/sync-results.py cell-type-nonETP-ALL-03 --bucket researcher-650251722463-us-east-2
 ```
 
-The final rds files for this module will be found in the S3 bucket, `s3://researcher-650251722463-us-east-2/cell-type-nonETP-ALL-03/results`, and the plots can be found in `s3://researcher-650251722463-us-east-2/cell-type-nonETP-ALL-03/plots/00-01_processing_rds`.
+The final rds files for this module will be found in the S3 bucket, `s3://researcher-650251722463-us-east-2/cell-type-nonETP-ALL-03/results/rds`, and the plots can be found in `s3://researcher-650251722463-us-east-2/cell-type-nonETP-ALL-03/plots`.
---
+++ analyses/cell-type-nonETP-ALL-03/scripts/02-03_annotation.R
@@ -0,0 +1,167 @@
+#!/usr/bin/env Rscript
+
+#This script uses ScType to perform cell type annotation and CopyKat for tumor cells identification.
+#We provides Azimuth_BM_level1.csv as the marker files for ScType, and generates sctype_classification metadata for the seurat object
+#We also provides the top 10 celltypes with the highest sctype score for each cluster in the results/.
+#Based on the annotated B cells, we provide them as the normal cells for CopyKat to identify tumor cells.
+#The outputs are final rds file and two umap plots showing cell type and tumor/normal cells status.
+
+lapply(c("dplyr","Seurat","HGNChelper","openxlsx"), library, character.only = T)
+library(copykat)
+library(ggplot2)
+
+# load gene set preparation function
+#source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/gene_sets_prepare.R")
+# load cell type annotation function
+source("https://raw.githubusercontent.com/IanevskiAleksandr/sc-type/master/R/sctype_score_.R")
+
+gene_sets_prepare <- function(path_to_db_file, cell_type){
+  cell_markers = openxlsx::read.xlsx(path_to_db_file)
+  cell_markers = cell_markers[cell_markers$tissueType == cell_type,] 
+  cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1); cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
+  
+  # correct gene symbols from the given DB (up-genes)
+  cell_markers$geneSymbolmore1 = sapply(1:nrow(cell_markers), function(i){
+    markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore1[i],",")))
+    markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""])
+    markers_all = sort(markers_all)
+    
+    if(length(markers_all) > 0){
+      suppressMessages({markers_all = unique(na.omit(markers_all))}) #since the markers are provided in Ensembl ID, I removed checkGeneSymbols function here 
+      paste0(markers_all, collapse=",")
+    } else {
+      ""
+    }
+  })
+  
+  # correct gene symbols from the given DB (down-genes)
+  cell_markers$geneSymbolmore2 = sapply(1:nrow(cell_markers), function(i){
+    markers_all = gsub(" ", "", unlist(strsplit(cell_markers$geneSymbolmore2[i],",")))
+    markers_all = toupper(markers_all[markers_all != "NA" & markers_all != ""])
+    markers_all = sort(markers_all)
+    
+    if(length(markers_all) > 0){
+      suppressMessages({markers_all = unique(na.omit(markers_all))}) #since the markers are provided in Ensembl ID, I removed checkGeneSymbols function here 
+      paste0(markers_all, collapse=",")
+    } else {
+      ""
+    }
+  })
+  
+  cell_markers$geneSymbolmore1 = gsub("///",",",cell_markers$geneSymbolmore1);cell_markers$geneSymbolmore1 = gsub(" ","",cell_markers$geneSymbolmore1)
+  cell_markers$geneSymbolmore2 = gsub("///",",",cell_markers$geneSymbolmore2);cell_markers$geneSymbolmore2 = gsub(" ","",cell_markers$geneSymbolmore2)
+  
+  gs = lapply(1:nrow(cell_markers), function(j) gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore1[j]),",")))); names(gs) = cell_markers$cellName
+  gs2 = lapply(1:nrow(cell_markers), function(j) gsub(" ","",unlist(strsplit(toString(cell_markers$geneSymbolmore2[j]),",")))); names(gs2) = cell_markers$cellName
+  
+  list(gs_positive = gs, gs_negative = gs2)
+}
+
+running_scType <- function(xls, tissue, annot.obj, assay = "RNA", thres = 4){
+  gs_list <- gene_sets_prepare(xls, tissue) #prepare gene sets
+  # check Seurat object version (scRNA-seq matrix extracted differently in Seurat v4/v5)
+  seurat_package_v5 <- isFALSE('counts' %in% names(attributes(annot.obj[[assay]])));
+  print(sprintf("Seurat object %s is used", ifelse(seurat_package_v5, "v5", "v4")))
+  # extract scaled scRNA-seq matrix
+  scRNAseqData_scaled <- if (seurat_package_v5) as.matrix(annot.obj[[assay]]$scale.data) else as.matrix(annot.obj[[assay]]@scale.data)
+  # run ScType
+  es.max <- sctype_score(scRNAseqData = scRNAseqData_scaled, scaled = TRUE, gs = gs_list$gs_positive, gs2 = gs_list$gs_negative)
+  # merge by cluster
+  cL_results <- do.call("rbind", lapply(unique(annot.obj@meta.data$leiden_clusters), function(cl){
+    es.max.cl = sort(rowSums(es.max[ ,rownames(annot.obj@meta.data[annot.obj@meta.data$leiden_clusters==cl, ])]), decreasing = !0)
+    head(data.frame(cluster = cl, type = names(es.max.cl), scores = es.max.cl, ncells = sum(annot.obj@meta.data$leiden_clusters==cl)), 10)
+  }))
+  sctype_scores <- cL_results %>% group_by(cluster) %>% top_n(n = 1, wt = scores)
+  # set low-confident (low ScType score) clusters to "unknown"
+  sctype_scores$type[as.numeric(as.character(sctype_scores$scores)) < sctype_scores$ncells/thres] <- "Unknown"
+  print(sctype_scores[,1:3])
+  
+  annot.obj@meta.data$sctype_classification = ""
+  for(j in unique(sctype_scores$cluster)){
+    cl_type = sctype_scores[sctype_scores$cluster==j,];
+    annot.obj@meta.data$sctype_classification[annot.obj@meta.data$leiden_clusters == j] = as.character(cl_type$type[1])
+  }
+  return (list(annot.obj, cL_results))
+}
+
+plot_modulescore <- function(xls, tissue, seu, sample.name){
+  gs_list <- gene_sets_prepare(xls, tissue) #prepare gene sets
+  for (i in 1:length(gs_list$gs_positive)){
+    seu <- AddModuleScore(object = seu, name = paste0(gsub(" ","",names(gs_list$gs_positive[i])),"_Features"), 
+                          features = list(gs_list$gs_positive[[i]]))
+  }
+  features.vec <- paste0(gsub(" ","",names(gs_list$gs_positive)),"_Features1")
+  Idents(seu) <- seu$leiden_clusters
+  DotPlot(seu, features = features.vec) + theme(axis.text.x = element_text(angle=45,hjust=1))  +
+    ggtitle(paste0(sample.name,": cell type module score")) +
+    scale_color_gradient2(low = scales::muted("blue"), mid = "whitesmoke", high = scales::muted("red"), midpoint = 0)
+  ggsave(file.path(out_loc,"plots",paste0(sample.name,"_features_dotplot.pdf")), width = 7, height = 7)
+}
+
+run_annot <- function(ind.sam){
+  seu <- readRDS(file.path(out_loc,"results/rds",paste0(ind.sam,".rds")))
+  final.obj <- seu #so I can keep an object without scale.data (it is taking too much space to save scale.data)
+  seu <- ScaleData(seu, features = rownames(seu))
+  ## relaxing the threshold of sctype_score larger than %ncells in a cluster, from 25% (default) to 10%
+  res <- running_scType(xls, tissue, seu, thres = 10)   #cell type annotation
+  seu$lowConfidence_annot <- res[[1]]$sctype_classification #seurat object with 'sctype_classification' metadata added
+  res <- running_scType(xls, tissue, seu)
+  seu <- res[[1]]
+  #res[[2]] - A table with top 10 cell types with the highest scores for each cluster
+  write.table(res[[2]], file = file.path(out_loc,"results",paste0(ind.sam,"_sctype_top10_celltypes_perCluster.txt")),
+              row.names = F, sep = "\t", quote = F)
+  p1 <- DimPlot(seu, reduction = "Xumap_", group.by = "sctype_classification", cols = ct_color) +
+    ggtitle(paste0(ind.sam,": cell type"))
+  p2 <- DimPlot(seu, reduction = "Xumap_", group.by = "lowConfidence_annot", cols = ct_color) +
+    ggtitle(paste0(ind.sam,": low confidence annotation"))
+  p1 + p2
+  ggsave(file.path(out_loc,"plots",paste0(ind.sam,"_celltype.pdf")), width = 10, height = 5)
+
+  plot_modulescore(xls, tissue, seu, ind.sam)
+  
+  #using copykat for tumor cells identification
+  norm.cells <- colnames(seu)[which(seu$sctype_classification=="B")] 
+  if (length(norm.cells) > 0){    #the sample has B cells annotated
+    copykat.test <- copykat(rawmat=seu@assays[["RNA"]]@counts, id.type="Ensemble",
+                            ngene.chr=5, win.size=25, KS.cut=0.1, sam.name=ind.sam,
+                            distance="euclidean", norm.cell.names=norm.cells,
+                            output.seg="FALSE", plot.genes="TRUE", genome="hg20",n.cores=50)
+    idx <- match(colnames(seu), copykat.test$prediction$cell.names)
+    seu$copykat.pred <- copykat.test$prediction$copykat.pred[idx]
+    DimPlot(seu, reduction = "Xumap_", group.by = "copykat.pred") +
+      ggtitle(paste0(ind.sam,": copykat prediction"))
+    ggsave(file.path(out_loc,"plots",paste0(ind.sam,"_copykatPred.pdf")), width = 7, height = 7)
+    
+    voi <- c('leiden_clusters','sctype_classification','lowConfidence_annot','copykat.pred')
+    final.obj$copykat.pred <- seu$copykat.pred
+  }else{
+    voi <- c('leiden_clusters','sctype_classification','lowConfidence_annot')
+  }
+  
+  write.table(data.frame(FetchData(seu, vars = voi)), sep = "\t", quote = F,
+              file = file.path(out_loc,"results",paste0(ind.sam,"_metadata.txt")))
+  final.obj$sctype_classification <- seu$sctype_classification
+  final.obj$lowConfidence_annot <- seu$lowConfidence_annot
+  saveRDS(final.obj, file = file.path(out_loc,"results/rds",paste0(ind.sam,".rds")))
+}
+
+
+project_root  <- rprojroot::find_root(rprojroot::is_git_root)
+projectID <- "SCPCP000003"
+out_loc <- file.path(project_root, "analyses/cell-type-nonETP-ALL-03")
+data_loc <- file.path(project_root, "data/current",projectID)
+
+metadata <- read.table(file.path(data_loc,"single_cell_metadata.tsv"), sep = "\t", header = T)
+metadata <- metadata[which(metadata$scpca_project_id == projectID &
+                             metadata$diagnosis == "Non-early T-cell precursor T-cell acute lymphoblastic leukemia"), ]
+sampleID <- metadata$scpca_sample_id
+# DB file
+db <- file.path(out_loc,"Azimuth_BM_level1.csv")
+xls <- openxlsx::buildWorkbook(read.csv(db, header = T))
+tissue <- "Immune system"  
+ct_color <- c("darkorchid","skyblue2","dodgerblue2","gold","beige","sienna1","green4","navy",
+              "chocolate4","red","darkred","#6A3D9A","maroon","yellow4","grey35","black","lightpink","grey80")
+names(ct_color) <- c("B","CD4 T","CD8 T","DC","HSPC","Mono","NK","Other T","Macrophage",
+                     "Early Eryth","Late Eryth","Plasma","Platelet","Stromal","Blast","Cancer","Pre Eryth","Unknown")
+
+purrr::walk(sampleID, run_annot)
