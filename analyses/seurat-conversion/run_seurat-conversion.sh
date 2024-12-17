398c2fe67b21145fdd4187d46c1394703fe53ce4
Joshua Shapiro <josh.shapiro@ccdatalab.org>
2024-12-17 13:09:28 -0500

    update run script

--- analyses/seurat-conversion/run_seurat-conversion.sh
+++ analyses/seurat-conversion/run_seurat-conversion.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 set -euo pipefail
 
-# Convert all files in an ScPCA project to Seurat format
-# Expects all projects to be found in the `data/current` directory, organized by project id
-# Outputs Seurat files to the `results/seurat` directory, organized by project id
+# Convert all files in the current data directory to Seurat format
+# Outputs Seurat files to the `results/seurat` directory, organized the same as the data directory
 
 # Set the working directory to the directory of this file
 cd "$(dirname "${BASH_SOURCE[0]}")"
@@ -11,9 +10,8 @@ cd "$(dirname "${BASH_SOURCE[0]}")"
 data_dir="../../data/current"
 output_dir="results/seurat"
 
-# get the project as the first arg
-project=$1
+# in case the output doesn't exist
+mkdir -p $output_dir
 
-mkdir -p $output_dir/$project
-
-Rscript scripts/convert-to-seurat.R --input_dir $data_dir/$project --output_dir $output_dir/$project
+# run the R script to convert all files
+Rscript scripts/convert-to-seurat.R --input_dir $data_dir --output_dir $output_dir
