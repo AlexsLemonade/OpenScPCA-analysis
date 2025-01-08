73d9498e3f3e4c85b69fd411f7b627a8c2aebf21
Ally Hawkins <ally.hawkins@ccdatalab.org>
2025-01-08 11:38:01 -0600

    shell script to assign cell types for all scpca projects

---
+++ analyses/cell-type-consensus/assign-consensus-celltypes.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+# This script is used to create a single table with cell type assignments all cells from all ScPCA samples  
+# The existing cell type annotations from SingleR and CellAssign are saved to a TSV file for each sample 
+# Then all TSV files are combined into a single file and consensus cell types are assigned 
+
+# Usage: ./evaluate-clusters.sh
+
+# to skip clustering and calculating metrics again: 
+# skip_metrics=1 ./evaluate-clusters.sh
+
+set -euo pipefail
+
+# navigate to where script lives
+cd $(dirname "$0")
+module_dir=$(pwd)
+
+data_dir="${module_dir}/../../data/current"
+# path to save consensus results 
+scpca_consensus_assignments_file="${module_dir}/results/scpca-consensus-celltype-assignments.tsv"
+# directory to store all individual tsv files 
+celltype_tsv_dir="${module_dir}/results/original-celltype-assignments" 
+mkdir -p ${celltype_tsv_dir}
+
+# define scripts and notebook directories
+scripts_dir="${module_dir}/scripts"
+
+# define reference input files 
+panglao_ref_file="${module_dir}/references/panglao-cell-type-ontologies.tsv"
+consensus_ref_file="${module_dir}/references/consensus-cell-type-reference.tsv"
+
+# run script to export tsv file on all processed objects
+for sce_file in $data_dir/*/SCPCS*/*_processed.rds; do
+
+    # define library ID
+    library_id=$(basename $sce_file | sed 's/_processed.rds$//')
+    
+    echo "Grabbing cell types for ${library_id}"
+    # get celltypes as tsv file 
+    Rscript $scripts_dir/03-save-coldata.R \
+      --sce_file $sce_file \
+      --output_file ${celltype_tsv_dir}/${library_id}_celltype-assignments.tsv
+
+done 
+
+echo "Combining TSVs and adding consensus labels"
+# run script to combine all tsv files and assign consensus cell types
+Rscript $scripts_dir/04-combine-celltype-tables.R \
+  --celltype_tsv_dir $celltype_tsv_dir \
+  --panglao_ref_file $panglao_ref_file \
+  --consensus_ref_file $consensus_ref_file \
+  --output_file $scpca_consensus_assignments_file
