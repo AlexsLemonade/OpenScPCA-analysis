f8e3574d37f5575b9f3e87fb5938ffe8e027051b
Maud Plaschka <100755946+maud-p@users.noreply.github.com>
2025-01-28 20:15:44 +0100

    Update analyses/cell-type-wilms-tumor-06/00_run_workflow.sh
    Co-authored-by: Stephanie Spielman <stephanie.spielman@gmail.com>

--- analyses/cell-type-wilms-tumor-06/00_run_workflow.sh
+++ analyses/cell-type-wilms-tumor-06/00_run_workflow.sh
@@ -161,7 +161,7 @@ done
 
 # Render notebook to make draft annotations
 Rscript -e "rmarkdown::render('${notebook_dir}/07_combined_annotation_across_samples_exploration.Rmd',
-                        params = list(predicted.celltype.threshold = ${predicted_celltype_threshold}, cnv_threshold_low = ${cnv_threhold_low}, cnv_threshold_high = ${cnv_threhold_high},  testing = ${TESTING}),
+                        params = list(predicted.celltype.threshold = ${predicted_celltype_threshold}, cnv_threshold_low = ${cnv_threshold_low}, cnv_threshold_high = ${cnv_threshold_high},  testing = ${TESTING}),
                         output_format = 'html_document',
                         output_file = '07_combined_annotation_across_samples_exploration.html',
                         output_dir = '${notebook_dir}')"
