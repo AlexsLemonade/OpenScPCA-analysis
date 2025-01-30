54d619fcc1954320153e8751fd7d200560c348da
Maud Plaschka <100755946+maud-p@users.noreply.github.com>
2025-01-30 15:48:48 +0100

    re-insert steps 00_ to 06a_ in the workflow

--- analyses/cell-type-wilms-tumor-06/00_run_workflow.sh
+++ analyses/cell-type-wilms-tumor-06/00_run_workflow.sh
@@ -56,7 +56,88 @@ else
   test_string=""
 fi
 
+# Download files used for label transfer
+# We'll define file names with absolute paths for robustness
 
+# First, download the fetal kidney reference (Stewart et al)
+kidney_ref_url="https://datasets.cellxgene.cziscience.com/fcb6225a-b329-4ac9-8a3f-559ca9bac50e.h5ad"
+kidney_ref_file="${PWD}/scratch/fetal_kidney.h5ad"
+kidney_ref_file_seurat="${PWD}/scratch/fetal_kidney_seurat.rds" # will be used by 00b
+if [[ ! -f $kidney_ref_file ]]; then
+  curl -o $kidney_ref_file $kidney_ref_url
+fi
+# Second, download the Seurat/Azimuth homologs file for gene ID conversion
+homologs_url="https://seurat.nygenome.org/azimuth/references/homologs.rds"
+homologs_file="${PWD}/scratch/homologs.rds"
+if [[ ! -f $homologs_file ]]; then
+  curl -o $homologs_file $homologs_url
+fi
+
+# Create the fetal references for label transfer (Stewart et al and Cao et al)
+Rscript scripts/prepare-fetal-references.R --kidney_ref_file "${kidney_ref_file}" --kidney_ref_file_seurat ${kidney_ref_file_seurat}
+
+# Run the label transfer and cluster exploration for all samples in the project
+for sample_dir in ${data_dir}/${project_id}/SCPCS*; do
+    sample_id=$(basename $sample_dir)
+
+    # define and create sample-specific directories
+    # directory for the pre-processed and labeled `Seurat` objects
+    results_dir=results/${sample_id}
+    # directory for sample-specific notebooks
+    sample_notebook_dir=notebook/${sample_id}
+
+    mkdir -p $results_dir
+    mkdir -p $sample_notebook_dir
+
+    # Pre-process the data - `Seurat` workflow
+    Rscript -e "rmarkdown::render('${notebook_template_dir}/01_seurat-processing.Rmd',
+                    params = list(scpca_project_id = '${project_id}', sample_id = '${sample_id}'),
+                    output_format = 'html_document',
+                    output_file = '01_seurat_processing_${sample_id}.html',
+                    output_dir = '${sample_notebook_dir}')"
+
+    # Label transfer from the Cao reference
+    Rscript -e "rmarkdown::render('${notebook_template_dir}/02a_label-transfer_fetal_full_reference_Cao.Rmd',
+                    params = list(scpca_project_id = '${project_id}', sample_id = '${sample_id}', homologs_file = '${homologs_file}', testing = ${TESTING}),
+                    output_format = 'html_document',
+                    output_file = '02a_fetal_all_reference_Cao_${sample_id}.html',
+                    output_dir = '${sample_notebook_dir}')"
+
+    # Label transfer from the Stewart reference
+    # Note that this reference has ensembl IDs
+    Rscript -e "rmarkdown::render('${notebook_template_dir}/02b_label-transfer_fetal_kidney_reference_Stewart.Rmd',
+                    params = list(scpca_project_id = '${project_id}', sample_id = '${sample_id}', testing = ${TESTING}),
+                    output_format = 'html_document',
+                    output_file = '02b_fetal_kidney_reference_Stewart_${sample_id}.html',
+                    output_dir = '${sample_notebook_dir}')"
+
+    # Cluster exploration
+    # This step does not directly contribute to the final annotations
+    if [[ $RUN_EXPLORATORY -eq 1 ]]; then
+      Rscript -e "rmarkdown::render('${notebook_template_dir}/03_clustering_exploration.Rmd',
+                      params = list(scpca_project_id = '${project_id}', sample_id = '${sample_id}', testing = ${TESTING}),
+                      output_format = 'html_document',
+                      output_file = '03_clustering_exploration_${sample_id}.html',
+                      output_dir = '${sample_notebook_dir}')"
+    fi
+done
+
+# These steps do not directly contribute to the final annotations
+if [[ $RUN_EXPLORATORY -eq 1 ]]; then
+
+  # Run notebook template to explore label transfer and clustering for all samples at once
+  for score_threshold in 0.5 0.75 0.85 0.95; do
+    Rscript -e "rmarkdown::render('${notebook_dir}/04_annotation_Across_Samples_exploration.Rmd',
+                    params = list(predicted.score_thr = ${score_threshold}, testing = ${TESTING}),
+                    output_format = 'html_document',
+                    output_file = '04_annotation_Across_Samples_exploration_predicted.score_threshold_${score_threshold}.html',
+                    output_dir = '${notebook_dir}')"
+  done
+
+fi
+
+# Prepare gene file for inferCNV
+Rscript scripts/06a_build-geneposition.R
 
 # Run infercnv for all samples with HMM i3 and using "both" as the reference, where possible
 for sample_dir in ${data_dir}/${project_id}/SCPCS*; do
