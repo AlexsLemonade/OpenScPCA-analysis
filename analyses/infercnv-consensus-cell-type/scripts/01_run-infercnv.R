9578a43b93644fb331c8968ed372c3060802633d
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-05-09 15:35:39 -0400

    back to cbind

--- analyses/infercnv-consensus-cell-type/scripts/01_run-infercnv.R
+++ analyses/infercnv-consensus-cell-type/scripts/01_run-infercnv.R
@@ -190,7 +190,7 @@ duplicated_cells <- intersect(colnames(ref_sce), colnames(sce))
 sce <- sce[, !(colnames(sce) %in% duplicated_cells)]
 
 # Create input matrix by combining library SCE and reference SCE counts assays
-raw_counts_matrix <- DelayedArray::cbind(
+raw_counts_matrix <- cbind(
   counts(sce),
   counts(ref_sce)
 )
