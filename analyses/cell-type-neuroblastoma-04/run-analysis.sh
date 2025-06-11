0d555cecf0b56eabff32e68f910f7f682a913e9e
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-06-11 15:09:56 -0400

    add testing flag and miscellaneous debugs

--- analyses/cell-type-neuroblastoma-04/run-analysis.sh
+++ analyses/cell-type-neuroblastoma-04/run-analysis.sh
@@ -15,7 +15,13 @@ set -euo pipefail
 module_dir=$(dirname "${BASH_SOURCE[0]}")
 cd ${module_dir}
 
+# Set up the testing flag
 testing=${testing:-0}
+if [[ $testing -eq 1 ]]; then
+    test_flag="--testing"
+else
+    test_flag=""
+fi
 
 # Define and create directories
 script_dir="scripts"
@@ -26,18 +32,21 @@ mkdir -p ${ref_dir}
 nbatlas_seurat="${ref_dir}/SeuratObj_Share_50kSubset_NBAtlas_v20240130.rds" # use the file name as recorded in Mendeley
 nbatlas_sce="${ref_dir}/NBAtlas_subset_sce.rds"
 nbatlas_anndata="${ref_dir}/NBAtlas_subset_anndata.h5ad"
-nbatlas_tumor_cells_file="${ref_dir}/NBAtlas_tumor_cells.tsv"
+
+# TODO: This needs to go on S3, and we need to sync it below
+nbatlas_tumor_metadata_file="${ref_dir}/SeuratMeta_Share_TumorZoom_NBAtlas_v20250228.rds"
 
 # First, download the NBAtlas Seurat object from: https://data.mendeley.com/datasets/yhcf6787yp/1
 # This link downloads the subsetted object with only 50K cells: SeuratObj_Share_50kSubset_NBAtlas_v20240130.rds
 nbatlas_url="https://data.mendeley.com/public-files/datasets/yhcf6787yp/files/0a569381-3a0c-4eec-863a-e20544b686ed/file_downloaded"
-if [[ ! -f $nbatlas_seurat ]]; thennbatlas_seurat
+if [[ ! -f $nbatlas_seurat ]]; then
   curl -o $nbatlas_seurat $nbatlas_url
 fi
 
 # Convert the Seurat object to SCE and AnnData formats
-Rscript ${script_dir}/00_convert-nbatlas.R
+Rscript ${script_dir}/00_convert-nbatlas.R \
     --seurat_file "${nbatlas_seurat}" \
     --tumor_metadata_file "${nbatlas_tumor_metadata_file}" \
     --sce_file "${nbatlas_sce}" \
-    --anndata_file "${nbatlas_anndata}"
+    --anndata_file "${nbatlas_anndata}" \
+    ${test_flag}
