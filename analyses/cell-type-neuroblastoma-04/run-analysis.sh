4284316bc9e6d708cfd7f989cfd94fd6ff6dc886
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-07-01 13:41:06 -0400

    more debugs

--- analyses/cell-type-neuroblastoma-04/run-analysis.sh
+++ analyses/cell-type-neuroblastoma-04/run-analysis.sh
@@ -58,17 +58,6 @@ scratch_dir="scratch"
 mkdir -p $ref_dir
 mkdir -p $scratch_dir
 
-# Define NBAtlas reference object files with the same name as on Mendeley: https://data.mendeley.com/datasets/yhcf6787yp/3
-nbatlas_full_seurat="${scratch_dir}/seuratObj_NBAtlas_share_v20241203.rds" # this is needed regardless of which nbatlas_version was specified since we need to subset ids
-nbatlas_tumor_metadata_file="${scratch_dir}/SeuratMeta_Share_TumorZoom_NBAtlas_v20250228.rds"
-
-# Define which NBAtlas version we're actually converting
-if [[ $nbatlas_version == "full" ]]; then
-  nbatlas_seurat=${nbatlas_full_seurat}
-else
-  nbatlas_seurat="${scratch_dir}/SeuratObj_Share_50kSubset_NBAtlas_v20240130.rds"
-fi
-
 nbatlas_sce="${ref_dir}/NBAtlas_${nbatlas_version}_sce.rds"
 nbatlas_anndata="${ref_dir}/NBAtlas_${nbatlas_version}_anndata.h5ad"
 
@@ -86,13 +75,27 @@ download_file() {
   fi
 }
 
-nbatlas_subset_url="https://data.mendeley.com/public-files/datasets/yhcf6787yp/files/0a569381-3a0c-4eec-863a-e20544b686ed/file_downloaded"
+# Define URLs; the first 2 will always be downloaded
 nbatlas_full_url="https://data.mendeley.com/public-files/datasets/yhcf6787yp/files/f5969395-5f6e-4c5d-a61a-5894773d0fee/file_downloaded"
 nbatlas_tumor_url="https://data.mendeley.com/public-files/datasets/yhcf6787yp/files/78cad1b4-7425-4073-ba09-362ef73c9ab9/file_downloaded"
+nbatlas_subset_url="https://data.mendeley.com/public-files/datasets/yhcf6787yp/files/0a569381-3a0c-4eec-863a-e20544b686ed/file_downloaded"
 
-download_file $nbatlas_subset_url $nbatlas_subset_seurat
+# Define NBAtlas reference object files with the same name as on Mendeley: https://data.mendeley.com/datasets/yhcf6787yp/3
+nbatlas_full_seurat="${scratch_dir}/seuratObj_NBAtlas_share_v20241203.rds" # this is needed regardless of which nbatlas_version was specified since we need to subset ids
+nbatlas_tumor_metadata_file="${scratch_dir}/SeuratMeta_Share_TumorZoom_NBAtlas_v20250228.rds"
+
+# Define which NBAtlas version we're actually converting, and download subset if it's that one
+if [[ $nbatlas_version == "full" ]]; then
+  nbatlas_seurat=${nbatlas_full_seurat}
+else
+  nbatlas_seurat="${scratch_dir}/SeuratObj_Share_50kSubset_NBAtlas_v20240130.rds"
+  download_file $nbatlas_subset_url $nbatlas_seurat
+fi
+
+# grab remaining downloads
 download_file $nbatlas_full_url $nbatlas_full_seurat
 download_file $nbatlas_tumor_url $nbatlas_tumor_metadata_file
+
 
 # Determine the ids to retain based on the *full* atlas object
 cell_id_file="${scratch_dir}/nbatlas-cell-ids.txt.gz"
