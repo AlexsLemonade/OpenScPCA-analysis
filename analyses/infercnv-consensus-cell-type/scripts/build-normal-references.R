c3904da2fa4da64dc6e8ba291a819bde81c88680
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-07-25 10:15:18 -0400

    script overhaul: use provided reference groups and output dir instead

--- analyses/infercnv-consensus-cell-type/scripts/build-normal-references.R
+++ analyses/infercnv-consensus-cell-type/scripts/build-normal-references.R
@@ -19,52 +19,52 @@ source(
   file.path(module_dir, "scripts", "build-normal-reference", "utils.R")
 )
 
+# Define a helper function to split strings
+split_input_str <- function(x) {
+  strsplit(x, ",")[[1]] |>
+    stringr::str_trim()
+}
+
 option_list <- list(
   make_option(
     opt_str = "--merged_sce_file",
     type = "character",
-    default = "../../data/current/results/merge-sce/SCPCP000004/SCPCP000004_merged.rds",
+    default = "",
     help = "Path to the merged SingleCellExperiment object for the given project"
   ),
   make_option(
-    opt_str = "--metadata_file",
+    opt_str = "--reference_celltype_group_file",
     type = "character",
-    default = "../../data/current/SCPCP000004/single_cell_metadata.tsv",
-    help = "Path to single_cell_metadata.tsv file for the given project"
+    default = "",
+    help = "Path to file mapping consensus cell types to reference cell type groups"
   ),
   make_option(
     opt_str = "--reference_groups",
     type = "character",
     default = "",
     help = "Comma-separated list of reference group names to build references for.
-      Use the string 'all-normal' to include a combined set of normal cell types, which can be further specifed with the `normal_reference_groups` argument."
+      Use the string 'normal' to include a combined set of normal cell types, which can be further specifed with the `normal_reference_groups` argument."
   ),
   make_option(
     opt_str = "--normal_reference_groups",
     type = "character",
     default = "",
-    help = "If 'all-normal' is provided to the `reference_groups` arguments, this argument can be used to specify which cell types to include in that category."
-  ),
-  make_option(
-    opt_str = "--reference_celltype_group_file",
-    type = "character",
-    default = file.path(module_dir, "references", "reference-cell-groups.tsv"),
-    help = "Path to file mapping consensus cell types to reference cell type groups"
-  ),
-  make_option(
-    opt_str = "--exclude_samples",
-    type = "character",
-    default = "",
-    help = "Comma-separated list of any samples which should be excluded from the references"
+    help = "Optional comma-separated list of reference group names to include in the 'normal' reference; only used when 'normal' is provided to the `reference_groups` argument. 
+    If not provided, all normal cell types will be included in the 'normal' grouping."
   ),
   make_option(
     opt_str = "--reference_output_dir",
     type = "character",
-    help = "Path to directory where normal reference files, named as `{reference cell group}.rds`,will be saved"
+    default = "",
+    help = "Path to directory where normal reference files, named as `{reference group}.rds`,will be saved"
+  ),
+  make_option(
+    opt_str = "--exclude_libraries",
+    type = "character",
+    default = "",
+    help = "Comma-separated list of any libraries which should be excluded from the references"
   )
 )
-
-
 
 # Parse options ----------------
 opts <- parse_args(OptionParser(option_list = option_list))
@@ -72,69 +72,65 @@ opts <- parse_args(OptionParser(option_list = option_list))
 # check input files and paths
 stopifnot(
   "merged_sce_file does not exist" = file.exists(opts$merged_sce_file),
-  "metadata_file does not exist" = file.exists(opts$metadata_file),
   "reference_celltype_group_file does not exist" = file.exists(opts$reference_celltype_group_file)
 )
 
 # read input data -----------
 celltype_group_df <- readr::read_tsv(opts$reference_celltype_group_file)
-
 merged_sce <- readRDS(opts$merged_sce_file)
 
-# keep only tumor libraries
-tumor_library_ids <- readr::read_tsv(opts$metadata_file) |>
-  dplyr::filter(!is_xenograft, !is_cell_line) |>
-  dplyr::pull(scpca_library_id)
+# Ensure is_xenograft and is_cell_line columns are logical
+# context: https://github.com/AlexsLemonade/scpcaTools/issues/302
+merged_sce$is_xenograft <- as.logical(merged_sce$is_xenograft)
+merged_sce$is_cell_line <- as.logical(merged_sce$is_cell_line)
 
-celltype_df <- merged_sce |>
-  colData() |>
+# Define reference groups -------
+reference_groups <- split_input_str(opts$reference_groups)
+normal_reference_groups <- split_input_str(opts$normal_reference_groups)
+
+# prepare data frame of all possible cells to consider -----------
+exclude_libraries <- split_input_str(opts$exclude_libraries)     
+
+cell_df <- colData(merged_sce) |>
   as.data.frame() |>
-  # consider only non-PDX
-  dplyr::filter(library_id %in% tumor_library_ids) |>
-  head()
-dplyr::select(barcodes, library_id, consensus_annotation = consensus_celltype_annotation) |>
-  dplyr::inner_join(celltype_group_df)
+  # remove PDX and cell lines
+  # remove any excluded ids passed in
+  dplyr::filter(
+    !is_xenograft,
+    !is_cell_line, 
+    !(library_id %in% exclude_libraries) 
+  ) |>
+  dplyr::select(cell_id, consensus_annotation = consensus_celltype_annotation) |>
+  dplyr::inner_join(celltype_group_df) 
 
-
-# Define vectors of cell ids for each reference -------
-
-# All-normal reference
-# as determined in ../../exploratory-notebooks/SCPCP000004/01_nb-consensus-cell-types.Rmd,
-# use the following groups in `all-normal`: endothelial, epithelial, adipocytes, immune
-normal_groups <- c("endothelial", "epithelial", "adipocytes", "immune")
-all_normal_ids <- celltype_df |>
-  dplyr::filter(reference_cell_group %in% normal_groups) |>
-  dplyr::pull(sce_cell_id)
-
-# Immune reference
-immune_ids <- celltype_df |>
-  dplyr::filter(reference_cell_group == "immune") |>
-  dplyr::pull(sce_cell_id)
-
-# Endothelial reference
-endo_ids <- celltype_df |>
-  dplyr::filter(reference_cell_group == "endothelial") |>
-  dplyr::pull(sce_cell_id)
-
-# Subset the SCE to create references -------------
-
-# first create the all-normal, and then the immune and endo as subsets from that one
-normal_reference <- merged_sce[, all_normal_ids] |>
-  clean_sce()
-
-# clean up for space
-rm(merged_sce)
-gc()
-
-# Ensure that the consensus cell type is recorded in reference SCE if not present
-if (!("consensus_annotation" %in% colnames(colData(normal_reference)))) {
-  colData(normal_reference) <- consensus_to_coldata(colData(normal_reference), celltype_df)
-}
-
-immune_reference <- normal_reference[, immune_ids]
-endo_reference <- normal_reference[, endo_ids]
-
-# Export references ---------
-readr::write_rds(normal_reference, opts$reference_normal_rds, compress = "gz")
-readr::write_rds(immune_reference, opts$reference_immune_rds, compress = "gz")
-readr::write_rds(endo_reference, opts$reference_endo_rds, compress = "gz")
+# create and export a reference SCE for each reference group -----
+reference_groups |>
+  purrr::walk(
+    \(ref_group) {
+      
+      output_file <- file.path(
+        opts$reference_output_dir,
+        glue::glue("{ref_group}.rds")
+      )
+      
+      # define cell groups to consider
+      if (ref_group == "normal") {
+        # use just provided normal types, or use all normal types
+        if (length(normal_reference_groups) == 0) {
+          all_groups <- uniue(cell_df$reference_cell_group)
+        } else {
+          all_groups <- normal_reference_groups
+        }
+      } else {
+        all_groups <- ref_group
+      }
+      
+      cell_ids <- cell_df |>
+        dplyr::filter(reference_cell_group %in% all_groups) |>
+        dplyr::pull(cell_id)
+      
+      # Subset and export
+      merged_sce[, cell_ids] |>
+        clean_sce() |>
+        readr::write_rds(output_file, compress = "gz")
+    })
