cf6ecdbf8ca739be0eef25fb354536ede11d772a
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-07-31 17:41:56 -0400

    script to perform label transfer on a given input object using the trained scanvi model from 03b

---
+++ analyses/cell-type-neuroblastoma-04/scripts/03c_run-scanvi-label-transfer.py
@@ -0,0 +1,160 @@
+#!/usr/bin/env python3
+
+# Script to perform label transfer using scANVI/scArches using the scANVI NBAtlas model on an ScPCA query object
+# This scANVI/scArches tutorial was used to help structure this script: https://docs.scvi-tools.org/en/1.3.2/tutorials/notebooks/multimodal/scarches_scvi_tools.html
+
+import os
+import sys
+from pathlib import Path
+import argparse
+import anndata
+import scvi
+import torch
+import pandas as pd
+from scipy.sparse import csr_matrix
+
+# Define constants
+
+BATCH_KEY = "Sample"
+COVARIATE_KEYS = [
+    "Assay",
+    "Platform",
+]
+CELL_ID_KEY = "cell_id"
+SCANVI_LATENT_KEY = "X_scANVI"
+SCANVI_PREDICTIONS_KEY = "scanvi_predicton"
+
+
+def main() -> None:
+    parser = argparse.ArgumentParser(
+        description="Annotate SCPCP000004 using scANVI/scArches label transfer with the NBAtlas reference.",
+    )
+    parser.add_argument(
+        "--query_file",
+        type=Path,
+        required=True,
+        help="Path to the input ScPCA AnnData file which has been prepared with scripts/03a_prepare-scanvi-query.R",
+    )
+    parser.add_argument(
+        "--reference_scanvi_model_dir",
+        type=Path,
+        required=True,
+        help="Path to the load the scANVI/scArches model trained on NBAtlas",
+    )
+    parser.add_argument(
+        "--query_scanvi_model_dir",
+        type=Path,
+        required=True,
+        help="Path to save the scANVI/scArches model trained with integrated query data",
+    )
+    parser.add_argument(
+        "--predictions_tsv",
+        type=Path,
+        required=True,
+        help="Path to the save TSV file of scANVI/scArches results on the query object, including predictions and the scANVI latent representation",
+    )
+    parser.add_argument(
+        "--testing",
+        action="store_true",
+        default=False,
+        help="Flag to use if running on test data and/or in CI",
+    )
+    parser.add_argument(
+        "--seed",
+        type=int,
+        default=2025,
+        help="Random seed to ensure reproducibility",
+    )
+    arg = parser.parse_args()
+
+    ################################################
+    ########### Input argument checks ##############
+    ################################################
+    arg_error = False
+
+    # Check that input files exist
+    if not os.path.isfile(arg.query_file):
+        print(
+            f"The provided input query file could not be found at: {arg.query_file}.",
+            file=sys.stderr,
+        )
+        arg_error = True
+    if not os.path.isdir(arg.reference_scanvi_model_dir):
+        print(
+            f"The provided reference scANVI model could not be found at: {arg.reference_scanvi_model_dir}.",
+            file=sys.stderr,
+        )
+        arg_error = True
+
+    # Set up testing and accelerator settings
+    if arg.testing:
+        # limit max_epochs for faster runtime and ensure CPU
+        common_train_kwargs = {"max_epochs": 5, "accelerator": "cpu"}
+    else:
+        accelerator = "gpu" if torch.cuda.is_available() else "cpu"
+        # don't use max_epochs; let scvi pick the heuristic
+        common_train_kwargs = {"accelerator": accelerator}
+
+    # Read and check that query object has expected columns
+    query = anndata.read_h5ad(arg.query_file)
+    expected_columns = [BATCH_KEY, CELL_ID_KEY] + COVARIATE_KEYS
+    all_present_query = all(col in query.obs.columns for col in expected_columns)
+    if not all_present_query:
+        print(
+            f"The query AnnData object is missing one or more expected columns: {expected_columns}.",
+            file=sys.stderr,
+        )
+        arg_error = True
+
+    # Exit if error(s)
+    if arg_error:
+        sys.exit(1)
+
+    # Set seed for reproducibility
+    # torch commands are ignored if GPU not present
+    scvi.settings.seed = arg.seed  # inherited by numpy and torch
+    torch.cuda.manual_seed(arg.seed)
+    torch.cuda.manual_seed_all(arg.seed)
+
+    # Load the trained scANVI model for label transfer
+    scanvi_model = scvi.model.SCANVI.load(arg.reference_scanvi_model_dir)
+
+    # Ensure anndata is using a sparse matrix for faster processing
+    query.X = csr_matrix(query.X)
+
+    ################################################
+    # Incorporate query data into the scANVI model #
+    ################################################
+
+    # Prepare query data for training
+    scvi.model.SCANVI.prepare_query_anndata(query, scanvi_model)
+    scanvi_query = scvi.model.SCANVI.load_query_data(query, scanvi_model)
+
+    # Train model and get latent dimensions, cell type predictions
+    scanvi_query.train(
+        **common_train_kwargs,
+        # additional scArches parameters
+        plan_kwargs={"weight_decay": 0.0},
+        check_val_every_n_epoch=5,
+    )
+    query.obsm[SCANVI_LATENT_KEY] = scanvi_query.get_latent_representation()
+    query.obs[SCANVI_PREDICTIONS_KEY] = scanvi_query.predict()
+
+    ################################################
+    ################ Export objects ################
+    ################################################
+
+    # Export the query-trained scANVI model
+    scanvi_query.save(arg.query_scanvi_model_dir, save_anndata=True, overwrite=True)
+
+    # Extract data frame of latent representation and predictions
+    latent_df = pd.DataFrame(query.obsm[SCANVI_LATENT_KEY])
+    latent_df = latent_df.rename(columns=lambda x: SCANVI_LATENT_KEY + "_" + str(x))
+    latent_df.index = query.obs.index # set index for joining
+    combined_df = latent_df.join(
+        query.obs[ expected_columns + [SCANVI_PREDICTIONS_KEY] ]
+    )
+    combined_df.to_csv(arg.predictions_tsv, sep="\t", index=False)
+
+if __name__ == "__main__":
+    main()
