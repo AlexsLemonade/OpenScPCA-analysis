8a465dd2618926f33f0f2a48231d811786da9b47
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-07-31 13:03:28 -0400

    comment to trigger styling

--- analyses/cell-type-neuroblastoma-04/scripts/03_run-scanvi.py
+++ analyses/cell-type-neuroblastoma-04/scripts/03_run-scanvi.py
@@ -15,12 +15,16 @@ from scipy.sparse import csr_matrix
 
 # Define variables to use in the objects
 BATCH_KEY = "Sample"
-COVARIATE_KEYS = ["Assay", "Platform"] # TODO: make this a script option to compare with/without?
+COVARIATE_KEYS = [
+    "Assay",
+    "Platform",
+]  # TODO: make this a script option to compare with/without?
 SCVI_LATENT_KEY = "X_scVI"
 SCANVI_LATENT_KEY = "X_scANVI"
 SCANVI_LABELS_KEY = "labels_scanvi"
 UNLABELED_VALUE = "Unknown"
 SCANVI_PREDICTIONS_KEY = "predictions_scanvi"
+
 
 def main() -> None:
     parser = argparse.ArgumentParser(
@@ -41,7 +45,7 @@ def main() -> None:
     parser.add_argument(
         "--reference_celltype_column",
         type=str,
-        default = "Cell_type_wImmuneZoomAnnot",
+        default="Cell_type_wImmuneZoomAnnot",
         help="Column in the reference AnnData object that contains cell type annotations."
         " Default is 'Cell_type_wImmuneZoomAnnot', unless --testing is specified in which case `Cell_type` is the default",
     )
@@ -127,10 +131,7 @@ def main() -> None:
     if arg.testing:
         cell_type_label = "Cell_type"
         # limit max_epochs for faster runtime and ensure CPU
-        common_train_kwargs = {
-            "max_epochs": 5,
-            "accelerator": "cpu"
-        }
+        common_train_kwargs = {"max_epochs": 5, "accelerator": "cpu"}
     else:
         arg.accelerator = arg.accelerator.lower()
         if arg.accelerator not in ["gpu", "cpu"]:
@@ -142,7 +143,7 @@ def main() -> None:
             arg_error = True
         if arg.accelerator == "gpu" and not torch.cuda.is_available():
             print(
-                "The specified accelerator is 'gpu', but no GPU is available.
+                "The specified accelerator is 'gpu', but no GPU is available.",
                 "Please use 'cpu' instead or ensure a GPU is available.",
                 file=sys.stderr,
             )
@@ -151,14 +152,15 @@ def main() -> None:
         # don't use max_epochs; let scvi pick the heuristic
         common_train_kwargs = {"accelerator": arg.accelerator}
 
-
     # Define lists of expected columns in the reference and query objects
     expected_covariate_columns = [BATCH_KEY] + COVARIATE_KEYS
     reference_expected_columns = expected_covariate_columns + [cell_type_label]
 
     # Read the reference and query objects and check that expected columns are present
     reference = anndata.read_h5ad(arg.reference_file)
-    all_present_reference = all(col in reference.obs.columns for col in reference_expected_columns)
+    all_present_reference = all(
+        col in reference.obs.columns for col in reference_expected_columns
+    )
     if not all_present_reference:
         print(
             f"The reference AnnData object is missing one or more expected columns: {reference_expected_columns}.",
@@ -167,7 +169,9 @@ def main() -> None:
         arg_error = True
 
     query = anndata.read_h5ad(arg.query_file)
-    all_present_query = all(col in query.obs.columns for col in expected_covariate_columns)
+    all_present_query = all(
+        col in query.obs.columns for col in expected_covariate_columns
+    )
     if not all_present_query:
         print(
             f"The query AnnData object is missing one or more expected columns: {expected_covariate_columns}.",
@@ -176,9 +180,15 @@ def main() -> None:
         arg_error = True
 
     # Prepare and check library ids to exclude_libraries if specified
-    exclude_libraries = [id.strip() for id in arg.exclude_libraries.split(",")] if arg.exclude_libraries else []
+    exclude_libraries = (
+        [id.strip() for id in arg.exclude_libraries.split(",")]
+        if arg.exclude_libraries
+        else []
+    )
     if len(exclude_libraries) > 0:
-        all_present = all([id in query.obs["library_id"].cat.categories for id in exclude_libraries])
+        all_present = all(
+            [id in query.obs["library_id"].cat.categories for id in exclude_libraries]
+        )
         if not all_present:
             print(
                 f"Not all libraries in `exclude_libraries` are present in the query object. Please check ids: {arg.exclude_libraries}.",
@@ -186,14 +196,13 @@ def main() -> None:
             )
             arg_error = True
 
-
+    # Exit if error(s)
     if arg_error:
         sys.exit(1)
 
-
     # Set seed for reproducibility
     # torch commands are ignored if GPU not present
-    scvi.settings.seed = arg.seed # inherited by numpy and torch
+    scvi.settings.seed = arg.seed  # inherited by numpy and torch
     torch.cuda.manual_seed(arg.seed)
     torch.cuda.manual_seed_all(arg.seed)
 
@@ -212,8 +221,8 @@ def main() -> None:
 
     scvi.model.SCVI.setup_anndata(
         reference,
-        batch_key = BATCH_KEY,
-        categorical_covariate_keys = COVARIATE_KEYS # control for cell/nucleus and 10x2/3
+        batch_key=BATCH_KEY,
+        categorical_covariate_keys=COVARIATE_KEYS,  # control for cell/nucleus and 10x2/3
     )
 
     scvi_model = scvi.model.SCVI(
@@ -221,7 +230,7 @@ def main() -> None:
         # scArches parameters
         use_layer_norm="both",
         use_batch_norm="none",
-        encode_covariates=True, # essential for scArches
+        encode_covariates=True,  # essential for scArches
         dropout_rate=0.2,
         n_layers=2,
     )
@@ -237,8 +246,8 @@ def main() -> None:
 
     scanvi_model = scvi.model.SCANVI.from_scvi_model(
         scvi_model,
-        unlabeled_category = UNLABELED_VALUE,
-        labels_key = SCANVI_LABELS_KEY,
+        unlabeled_category=UNLABELED_VALUE,
+        labels_key=SCANVI_LABELS_KEY,
     )
     scanvi_model.train(**common_train_kwargs)
     reference.obsm[SCANVI_LATENT_KEY] = scanvi_model.get_latent_representation()
@@ -270,27 +279,29 @@ def main() -> None:
         ["NBAtlas", "ScPCA"]
     )
     # add the final predictions into the integrated data object
-    adata_integrated.obs[SCANVI_PREDICTIONS_KEY] = scanvi_query.predict(adata_integrated)
+    adata_integrated.obs[SCANVI_PREDICTIONS_KEY] = scanvi_query.predict(
+        adata_integrated
+    )
 
     # Export the NBAtlas-trained SCVI model
-    scvi_model.save(arg.reference_scvi_model_dir, save_anndata = True, overwrite = True)
+    scvi_model.save(arg.reference_scvi_model_dir, save_anndata=True, overwrite=True)
 
     # Export the NBAtlas-trained scANVI model
-    scanvi_model.save(arg.reference_scanvi_model_dir, save_anndata = True, overwrite = True)
+    scanvi_model.save(arg.reference_scanvi_model_dir, save_anndata=True, overwrite=True)
 
     # Export the query-trained scANVI model
-    scanvi_query.save(arg.query_scanvi_model_dir, save_anndata = True, overwrite = True)
+    scanvi_query.save(arg.query_scanvi_model_dir, save_anndata=True, overwrite=True)
 
     # Save the full integrated object with labels and scANVI latent representations
     adata_integrated.write(arg.integrated_scanvi_anndata)
 
     # Save TSV of the full scANVI latent representation, covariates, and predictions
     latent_df = pd.DataFrame(adata_integrated.obsm[SCANVI_LATENT_KEY])
-    latent_df = latent_df.rename(columns = lambda x: SCANVI_LATENT_KEY + "_" +str(x))
+    latent_df = latent_df.rename(columns=lambda x: SCANVI_LATENT_KEY + "_" + str(x))
     latent_df.index = adata_integrated.obs.index
 
     combined_df = latent_df.join(adata_integrated.obs)
-    combined_df.to_csv(arg.predictions_tsv, sep='\t', index = False)
+    combined_df.to_csv(arg.predictions_tsv, sep="\t", index=False)
 
 
 if __name__ == "__main__":
