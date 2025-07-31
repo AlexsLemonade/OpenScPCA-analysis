13166264588056eba9f257495de17c5c1cdd5e8d
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-07-31 17:41:37 -0400

    updates to remove extraneous code and improve file exports

--- analyses/cell-type-neuroblastoma-04/scripts/03b_train-scanvi.py
+++ analyses/cell-type-neuroblastoma-04/scripts/03b_train-scanvi.py
@@ -10,19 +10,17 @@ import argparse
 import anndata
 import scvi
 import torch
+import pandas as pd
 from scipy.sparse import csr_matrix
 
-# Define variables to use in the objects
+# Define constants
 BATCH_KEY = "Sample"
 COVARIATE_KEYS = [
     "Assay",
     "Platform",
-]  # TODO: make this a script option to compare with/without?
-SCVI_LATENT_KEY = "X_scVI"
+]
+CELL_ID_KEY = "cell_id"
 SCANVI_LATENT_KEY = "X_scANVI"
-SCANVI_LABELS_KEY = "labels_scanvi"
-UNLABELED_VALUE = "Unknown"
-SCANVI_PREDICTIONS_KEY = "predictions_scanvi"
 
 
 def main() -> None:
@@ -46,13 +44,19 @@ def main() -> None:
         "--reference_scvi_model_dir",
         type=Path,
         required=True,
-        help="Path to the save the intermediate SCVI model trained on the reference object",
+        help="Path to save the intermediate SCVI model trained on the reference object",
     )
     parser.add_argument(
         "--reference_scanvi_model_dir",
         type=Path,
         required=True,
-        help="Path to the save the scANVI model trained on the reference object",
+        help="Path to save the scANVI model trained on the reference object",
+    )
+    parser.add_argument(
+        "--scanvi_latent_tsv",
+        type=Path,
+        required=True,
+        help="Path to save a standalone TSV of the scANVI model latent representation on the reference object",
     )
     parser.add_argument(
         "--testing",
@@ -82,25 +86,21 @@ def main() -> None:
         arg_error = True
 
     # Set up testing and accelerator settings
-    # Note we do this here so `cell_type_label` can be used to check the reference object next
     if arg.testing:
-        cell_type_label = "Cell_type"
         # limit max_epochs for faster runtime and ensure CPU
         common_train_kwargs = {"max_epochs": 5, "accelerator": "cpu"}
+        cell_type_column = "Cell_type"
     else:
-        cell_type_label = arg.reference_celltype_column
-        accelerator = "gpu" if torch.cuda.is_available() else "cpu"
         # don't use max_epochs; let scvi pick the heuristic
-        common_train_kwargs = {"accelerator": accelerator}
+        common_train_kwargs = {
+            "accelerator": "gpu" if torch.cuda.is_available() else "cpu"
+        }
+        cell_type_column = arg.reference_celltype_column
 
-    # Define lists of expected columns in the reference and query objects
-    expected_columns = [BATCH_KEY, cell_type_label] + COVARIATE_KEYS
-
-    # Read the reference and query objects and check that expected columns are present
+    # Read and check that the reference object has expected columns
+    expected_columns = [BATCH_KEY, CELL_ID_KEY, cell_type_column] + COVARIATE_KEYS
     reference = anndata.read_h5ad(arg.reference_file)
-    all_present_reference = all(
-        col in reference.obs.columns for col in expected_columns
-    )
+    all_present_reference = all(col in reference.obs.columns for col in expected_columns)
     if not all_present_reference:
         print(
             f"The reference AnnData object is missing one or more expected columns: {expected_columns}.",
@@ -118,12 +118,12 @@ def main() -> None:
     torch.cuda.manual_seed(arg.seed)
     torch.cuda.manual_seed_all(arg.seed)
 
+    # Ensure anndata is using a sparse matrix for faster processing
+    reference.X = csr_matrix(reference.X)
+
     ################################################
     ######## SCVI reference model training #########
     ################################################
-
-    # Ensure anndata objects are using sparse matries for faster scvi processing
-    reference.X = csr_matrix(reference.X)
 
     scvi.model.SCVI.setup_anndata(
         reference,
@@ -141,19 +141,17 @@ def main() -> None:
         n_layers=2,
     )
 
+    # Train SCVI model
     scvi_model.train(**common_train_kwargs)
-    reference.obsm[SCVI_LATENT_KEY] = scvi_model.get_latent_representation()
 
     ################################################
     ####### scANVI reference model training ########
     ################################################
 
-    reference.obs[SCANVI_LABELS_KEY] = reference.obs[cell_type_label].values
-
     scanvi_model = scvi.model.SCANVI.from_scvi_model(
         scvi_model,
-        unlabeled_category=UNLABELED_VALUE,
-        labels_key=SCANVI_LABELS_KEY,
+        unlabeled_category="Unknown", # will be used to set up query next; labels will start as `Unknown`
+        labels_key=cell_type_column,
     )
     scanvi_model.train(**common_train_kwargs)
     reference.obsm[SCANVI_LATENT_KEY] = scanvi_model.get_latent_representation()
@@ -167,6 +165,14 @@ def main() -> None:
 
     # Export the NBAtlas-trained scANVI model
     scanvi_model.save(arg.reference_scanvi_model_dir, save_anndata=True, overwrite=True)
+
+    # Export standalone TSV of NBAtlas scANVI latent representation & labels
+    obs_cols = expected_columns + ["cell_id", SCANVI_LATENT_KEY, cell_type_column]
+    latent_df = pd.DataFrame(reference.obsm[SCANVI_LATENT_KEY])
+    latent_df = latent_df.rename(columns=lambda x: SCANVI_LATENT_KEY + "_" + str(x))
+    latent_df.index = reference.obs.index # set index for joining
+    combined_df = latent_df.join(reference.obs[expected_columns]) # only save relevant columns here
+    combined_df.to_csv(arg.scanvi_latent_tsv, sep="\t", index=False)
 
 if __name__ == "__main__":
     main()
