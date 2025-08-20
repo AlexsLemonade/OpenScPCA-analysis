9f29095db6ca49ddb4745c51cb50e04b2acbca41
Stephanie Spielman <stephanie.spielman@gmail.com>
2025-08-20 10:19:15 -0400

    account for other solid tumors

--- analyses/infercnv-consensus-cell-type/scripts/prepare-diagnosis-map.R
+++ analyses/infercnv-consensus-cell-type/scripts/prepare-diagnosis-map.R
@@ -11,15 +11,22 @@ diagnosis_map <- readr::read_tsv(diagnosis_grouping_url)
 updated_diagnosis_map <- diagnosis_map |>
   # remove non-cancer
   dplyr::filter(ontology_id != "PATO:0000461") |>
-  # split up leukemias based on cell types
+  # split up leukemias based on cell types in their ontology name
+  # set the "Other solid tumors" to have their ontology diagnosis be the group
   dplyr::mutate(
     diagnosis_group = dplyr::case_when(
       diagnosis_group == "Leukemia" & stringr::str_detect(human_readable_value, "T-cell") ~ "T-cell leukemia",
       diagnosis_group == "Leukemia" & stringr::str_detect(human_readable_value, "B-cell") ~ "B-cell leukemia",
-      diagnosis_group == "Leukemia" & stringr::str_detect(human_readable_value, "myeloid") ~ "AML",
+      diagnosis_group == "Leukemia" & stringr::str_detect(human_readable_value, "myeloid") ~ "Myeloid leukemia",
       diagnosis_group == "Leukemia" & stringr::str_detect(human_readable_value, "Mixed") ~ "Mixed-type leukemia",
+      diagnosis_group == "Other solid tumors" ~ human_readable_value,
       .default = diagnosis_group
-    )) 
+    )) |>
+  dplyr::arrange(diagnosis_group) |>
+  # but keep leukemias together
+  dplyr::arrange(stringr::str_detect(diagnosis_group, "leukemia"))
+  
+  
 
 # Export diagnosis map ------------
 readr::write_tsv(updated_diagnosis_map, "../references/broad-diagnosis-map.tsv")
