314b4899d5179644cb6f46a2fda0fccb7a0bddbd
Stephanie J. Spielman <stephanie.spielman@gmail.com>
2026-08-05 08:07:37 -0400

    add script, small for now, to convert objects to SCE

---
+++ analyses/cell-type-osteosarcoma/scripts/convert-osteocar.R
@@ -0,0 +1,54 @@
+#!/usr/bin/env Rscript
+#
+# Export SCE reference versions
+
+# sets limit to 48 GB, needed for reading in the qs2 files
+mem.maxVSize(48000)  
+
+library(optparse)
+
+option_list <- list(
+  make_option(
+    opt_str = c("--input_ref_file"),
+    type = "character",
+    default = "",
+    help = "Path to Seurat version of an OsteoCar object in qs2 format"
+  ),
+  make_option(
+    opt_str = c("--output_sce_file"),
+    type = "character",
+    help = "Path to RDS file with an SCE version of the reference."
+  )
+)
+
+# Parse options and check arguments
+opts <- parse_args(OptionParser(option_list = option_list))
+
+stopifnot("input_ref_file does not exist" = file.exists(opts$input_ref_file))
+
+# load the bigger libraries after passing checks
+suppressPackageStartupMessages({
+  library(Seurat)
+  library(SingleCellExperiment)
+})
+
+
+# export reformatted object if requested
+if (!is.null(opts$output_sce_file) && !file.exists(opts$output_sce_file)) {
+  # read input files and determine relevant cell ids
+  osteocar_seurat <- qs2::qs_read(opts$input_ref_file)
+
+  # convert to SCE, using `as` to avoid CI error
+  osteocar_sce <- as.SingleCellExperiment(osteocar_seurat)
+  rm(osteocar_seurat)
+  gc()
+
+  # remove unneeded items from object
+  reducedDims(osteocar_sce) <- NULL
+
+  readr::write_rds(
+    osteocar_sce,
+    opts$output_sce_file,
+    compress = "gz"
+  )
+}
